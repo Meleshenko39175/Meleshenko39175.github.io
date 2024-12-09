f62cc1e0343b9afa27b2ffd88df4576dc3735019
Ryan Meleshenko <meleshenko39175@students.apsb.org>
2024-12-09 15:01:16 +0000

    add runtime project

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -18,8 +18,58 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+    function createSawBlade(posx, posy, size = 25)
+    {
+      var hitZoneSize = size;
+      var damageFromObstacle = 10;
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+      sawBladeHitZone.x = posx;
+      sawBladeHitZone.y = posy;
+      game.addGameItem(sawBladeHitZone);
 
-    
+      var obstacleImage = draw.bitmap("img/sawblade.png");
+      obstacleImage.x = -size
+      obstacleImage.y = -size
+      sawBladeHitZone.addChild(obstacleImage);
+    }
+
+    function createEnemy(posx, posy = 25)
+    {
+      var enemy = game.createGameItem("enemy", 25);
+      var redSquare = draw.rect(50, 50, "red");
+      redSquare.x = -25;
+      redSquare.y = -25;
+      enemy.addChild(redSquare);
+      enemy.x = posx
+      enemy.y = groundY - posy
+      enemy.velocityX = -10
+      enemy.rotationalVelocity = 10
+      enemy.onPlayerCollision = function()
+      {
+        game.changeIntegrity(-10)
+      };
+      enemy.onProjectileCollision = function()
+      {
+        game.increaseScore(100);
+        enemy.shrink();
+
+      };
+      game.addGameItem(enemy)
+
+      var obstacleImage = draw.bitmap("img/sawblade.png");
+      obstacleImage.x = -25
+      obstacleImage.y = -25
+      enemy.addChild(obstacleImage);
+    }
+    createSawBlade(400, 300)
+    createSawBlade(800, 300)
+    createSawBlade(1200, 300, 50)
+    createEnemy(1000)
+    createEnemy(1400)
+    for (var i = 0; i < 25; i++)
+    {
+      createEnemy(3000 + (i*5))
+    }
 
     function startLevel() {
       // TODO 13 goes below here
